c2e52d1162823b380c6c7d17a63a1e13820f46d3
rdsoftllp <rajesh@rdsoftllp.com>
2022-09-10 10:47:04 +0530

    wip

---
+++ template/src/translations/resources/en.ts
@@ -0,0 +1,9 @@
+export default {
+    welcome: 'Welcome to react native boilerplate by rdsoftllp',
+    login: {
+        loginButton: 'Login'
+    },
+    home: {
+        welcome: 'Welcome {{name}}'
+    }
+}
